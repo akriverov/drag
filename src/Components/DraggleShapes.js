163c7c704c92d6635abc9dede129abb195ecb8fe
ana <ak.velasquez@duocuc.cl>
2023-10-27 23:26:41 -0300

    Arreglo de figuras

--- src/Components/DraggleShapes.js
+++ src/Components/DraggleShapes.js
@@ -4,7 +4,6 @@ import defaultButtons from "../EditMenu/defaultButtons";
 import EditItem from "./DDEditor/EditItem";
 import ColorPicker from "../utils/ui/ColorPicker";
 import { useState } from "react";
-<<<<<<< HEAD
 
 const shapes = {
   square: (color, stroke) => <SquareIcon color={color} stroke={stroke} />,
@@ -15,11 +14,6 @@ const shapes = {
   diamond: (color, stroke) => <DiamondIcon color={color} stroke={stroke} />,
 };
 
-=======
-const shapes = {
-  square: (color, stroke) => <StarIcon color={color} stroke={stroke}/>
-}
->>>>>>> 2eb0fc033d6258baf303b58a8d57fa9e545b25da
 function DraggableShape(props) {
   const { elemData } = props;
   const siteData = useContext(SiteContext);
@@ -143,20 +137,15 @@ function DraggableShape(props) {
         renderPanel={PanelControls}
         mode={mode}
       >
-<<<<<<< HEAD
         {shapes[elemData.name] &&
           shapes[elemData.name](
             elemData.style.backgroundColor,
             elemData.style.borderColor
           )}
-=======
-          {shapes[elemData.name] && shapes[elemData.name](elemData.style.backgroundColor, elemData.style.borderColor)}
->>>>>>> 2eb0fc033d6258baf303b58a8d57fa9e545b25da
       </EditItem>
     </>
   );
 }
-<<<<<<< HEAD
 /////FIGURAS///////
 const SquareIcon = ({ color = "black", stroke = "black" }) => {
   return (
@@ -258,19 +247,3 @@ const DiamondIcon = ({ color = "black", stroke = "black" }) => {
 };
 
 export default DraggableShape;
-=======
-const StarIcon = ({ color = 'black', stroke = 'black' }) => {
-  return (
-    <svg height="100%" width="100%" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"  
-	 viewBox="0 0 47.94 47.94" >
-      <path fill={color} stroke={stroke} d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757
-        c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042
-        c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685
-        c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528
-        c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956
-        C22.602,0.567,25.338,0.567,26.285,2.486z"/>
-    </svg>
-  )
-}
-export default DraggableShape;
->>>>>>> 2eb0fc033d6258baf303b58a8d57fa9e545b25da
